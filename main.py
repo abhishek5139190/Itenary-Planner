ade7c18278ee99145ab124b7339722a6a262d8dd
Abhishek Mishra <abhishek5139190@users.noreply.github.com>
2025-02-11 17:40:48 +0530

    Update main.py
    Color Change

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ with col2:
         # Ensure text is inside a scrollable container
         st.markdown(
             f"""
-            <div style="height: 500px; width: 150%; overflow-y: auto; padding: 10px; background-color: #E3E3E3; border: 1px solid #E3E3E3;">
+            <div style="height: 500px; width: 120%; overflow-y: auto; padding: 10px; background-color: #f9f9f9; border: 1px solid #f9f9f9;">
                 {travel.content}
             </div>
             """,
